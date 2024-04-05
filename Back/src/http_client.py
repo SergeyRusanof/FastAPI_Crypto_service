9b879a9954b743cc9620528916e172792cad2315
sergey <sergeyrusanov25@gmail.com>
2024-04-05 20:01:19 +0300

    first test

--- Back/src/http_client.py
+++ Back/src/http_client.py
@@ -7,3 +7,12 @@ class HTTPClient:
         self._session = ClientSession(
             base_url=base_url,
             headers={'X-CMC_PRO_API_KEY': settings.CMC_API_KEY})
+        
+#pro-api.coinmarketcap.com
+
+class CMCHTPClient(HTTPClient):
+    async def get_listings(self):
+        async with self._session.get('/v1/cryptocurrency/listings/latest') as resp:
+            result = await resp.json('data')
+            return result['data']
+        
